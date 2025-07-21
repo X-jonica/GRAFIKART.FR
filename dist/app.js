378200e882c1ae08e866d3678304a5f72b51be29
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-07-21 10:47:18 +0300

    les types de nabe en typescript

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,29 @@
-const compteur = document.querySelector("#compteur");
-let i = 0;
+"use strict";
+// DECLARATION AVEC TYPESCRIPT
+const a = "Hello";
+const n = 3;
+const b = true;
+const d = null;
+const arr = ["hello", "world"];
+const all = ["bonjour", 2, true, "tous le monde"]; //avec 'any' on peux mettre tous dans  une variable ou tableau 
+const user = { firstname: "John", lastname: "doe" }; //le point d'interrogation veux dire otionnel
+const voiture = { marque: "Mercedes", couleur: "noire", modele: "recent" };
+const date = new Date();
+const db = (e) => {
+    return 3;
+}; //le mot cle "void" signifie qu on se fous completemlent des retours 
+function printId(id) {
+    console.log(id.toString());
+}
+/*
+const compteur = document.querySelector("#compteur")
+let i = 0
+
 const increment = (e) => {
-    i++;
-    compteur.querySelector("span").innerText = i.toString();
-};
-compteur.addEventListener("click", increment);
+    i++
+    compteur.querySelector("span").innerText = i.toString()
+}
+
+compteur.addEventListener("click", increment)
+
+*/ 
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,25 @@
+// DECLARATION AVEC TYPESCRIPT
+
+const a:string = "Hello"
+const n:number = 3
+const b:boolean = true
+const d:null = null
+const arr:string[] = ["hello", "world"]
+const all:any[] = ["bonjour", 2, true, "tous le monde"] //avec 'any' on peux mettre tous dans  une variable ou tableau 
+const user:{firstname:string, lastname?: string} = {firstname:"John", lastname:"doe"} //le point d'interrogation veux dire otionnel
+const voiture:{marque: string, [key: string]: string} = {marque: "Mercedes", couleur: "noire", modele:"recent"}
+const date:Date = new Date()
+
+const db:(e: MouseEvent) => void = (e: MouseEvent): number => {
+ return 3
+} //le mot cle "void" signifie qu on se fous completemlent des retours 
+
+
+function printId(id: number):void {
+    console.log(id.toString());
+}
+
+/*
 const compteur = document.querySelector("#compteur")
 let i = 0
 
@@ -7,3 +29,5 @@ const increment = (e) => {
 }
 
 compteur.addEventListener("click", increment)
+
+*/
