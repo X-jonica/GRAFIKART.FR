b691f094baeaaa09a770a12ef2ed327ee32a0cfe
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-07-21 11:09:14 +0300

    type de base terminé , nous allons maintenant voir le: Narrowing dansle video 4

--- dist/app.js
+++ dist/app.js
@@ -5,25 +5,33 @@ const n = 3;
 const b = true;
 const d = null;
 const arr = ["hello", "world"];
-const all = ["bonjour", 2, true, "tous le monde"]; //avec 'any' on peux mettre tous dans  une variable ou tableau 
-const user = { firstname: "John", lastname: "doe" }; //le point d'interrogation veux dire otionnel
-const voiture = { marque: "Mercedes", couleur: "noire", modele: "recent" };
+const all = ["bonjour", 2, true, "tous le monde"]; //avec 'any' on peux mettre tous dans  une variable ou tableau
+const user = {
+    firstname: "John",
+    lastname: "doe",
+}; //le point d'interrogation veux dire otionnel
+const voiture = {
+    marque: "Mercedes",
+    couleur: "noire",
+    modele: "recent",
+};
 const date = new Date();
 const db = (e) => {
     return 3;
-}; //le mot cle "void" signifie qu on se fous completemlent des retours 
+}; //le mot cle "void" signifie qu on se fous completemlent des retours
 function printId(id) {
     console.log(id.toString());
 }
-/*
-const compteur = document.querySelector("#compteur")
-let i = 0
-
+// il se peut que typescrit ne connais pas le tye d un element
+// const compteur = document.querySelector("#compteur") as HTMLButtonElement; //  c' estpour indiquer a Typescreipt qu il s'agit d une Buutton HtML
+const compteur = document.querySelector("#compteur");
+let i = 0;
 const increment = (e) => {
-    i++
-    compteur.querySelector("span").innerText = i.toString()
-}
-
-compteur.addEventListener("click", increment)
-
-*/ 
+    e.preventDefault();
+    i++;
+    const span = compteur === null || compteur === void 0 ? void 0 : compteur.querySelector("span");
+    if (span) {
+        span.innerText = i.toString();
+    }
+};
+compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener("click", increment); //ajouter une "addEventListener" si compteur existe
--- src/app.ts
+++ src/app.ts
@@ -1,33 +1,44 @@
 // DECLARATION AVEC TYPESCRIPT
 
-const a:string = "Hello"
-const n:number = 3
-const b:boolean = true
-const d:null = null
-const arr:string[] = ["hello", "world"]
-const all:any[] = ["bonjour", 2, true, "tous le monde"] //avec 'any' on peux mettre tous dans  une variable ou tableau 
-const user:{firstname:string, lastname?: string} = {firstname:"John", lastname:"doe"} //le point d'interrogation veux dire otionnel
-const voiture:{marque: string, [key: string]: string} = {marque: "Mercedes", couleur: "noire", modele:"recent"}
-const date:Date = new Date()
+const a: string = "Hello";
+const n: number = 3;
+const b: boolean = true;
+const d: null = null;
+const arr: string[] = ["hello", "world"];
+const all: any[] = ["bonjour", 2, true, "tous le monde"]; //avec 'any' on peux mettre tous dans  une variable ou tableau
+const user: { firstname: string; lastname?: string } = {
+   firstname: "John",
+   lastname: "doe",
+}; //le point d'interrogation veux dire otionnel
+const voiture: { marque: string; [key: string]: string } = {
+   marque: "Mercedes",
+   couleur: "noire",
+   modele: "recent",
+};
+const date: Date = new Date();
 
-const db:(e: MouseEvent) => void = (e: MouseEvent): number => {
- return 3
-} //le mot cle "void" signifie qu on se fous completemlent des retours 
+const db: (e: MouseEvent) => void = (e: MouseEvent): number => {
+   return 3;
+}; //le mot cle "void" signifie qu on se fous completemlent des retours
 
-
-function printId(id: number):void {
-    console.log(id.toString());
+function printId(id: number | string): void {
+   console.log(id.toString());
 }
 
-/*
-const compteur = document.querySelector("#compteur")
+// il se peut que typescrit ne connais pas le tye d un element
+// const compteur = document.querySelector("#compteur") as HTMLButtonElement; //  c' estpour indiquer a Typescreipt qu il s'agit d une Buutton HtML
+
+
+const compteur = document.querySelector("#compteur") as HTMLElement
 let i = 0
 
-const increment = (e) => {
+const increment = (e: Event) => {
+    e.preventDefault()
     i++
-    compteur.querySelector("span").innerText = i.toString()
+    const span = compteur?.querySelector("span")
+    if(span){
+        span.innerText = i.toString()
+    }
 }
 
-compteur.addEventListener("click", increment)
-
-*/
+compteur?.addEventListener("click", increment) //ajouter une "addEventListener" si compteur existe
