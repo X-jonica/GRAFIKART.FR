b902428d92c8dfe9e2406164bfc601b47161860f
X-jonica <henintsoahasimanitriniainajoni@gmail.com>
2025-07-21 12:47:32 +0300

    voila le narrowing c est compris

--- dist/app.js
+++ dist/app.js
@@ -35,3 +35,40 @@ const increment = (e) => {
     }
 };
 compteur === null || compteur === void 0 ? void 0 : compteur.addEventListener("click", increment); //ajouter une "addEventListener" si compteur existe
+// le NARROWING : typescrit va devinner en foncstion du paramettre qu on a attribuer a une fonction
+function displayId(id) {
+    if (typeof id === "number") {
+        console.log(id * 3);
+    }
+    else {
+        console.log(id.toUpperCase()); //ici typescript sait que a est un string car dans le prochain conditon c etait deja un number
+    }
+}
+// deuxieme exemple 
+function exemple(a, b) {
+    if (a === b) {
+        console.log(a); //doc ici typpescript va deiner que a serat soit "string | boolean" car ce sont les deux intercection entre a et b
+    }
+}
+// troisieme exemple cas d un tableau
+function autreExemple(a) {
+    if (Array.isArray(a)) {
+        return a[0]; //tyescript va comprendre que a est ici un tableau
+    }
+    return;
+}
+// prochain exemple 
+function prochainExemple(a) {
+    if ("value" in a) {
+        console.log(a); //ici typescript va comrendre que a est une "HTMLinputelement parce que value se truve dans une lement inpu en HTML "
+    }
+}
+// prochaine autre exemple 
+function isDate(a) {
+    return a instanceof Date;
+}
+function example(a) {
+    if (isDate(a)) {
+        a; //c est logique que a ici est une Date
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -28,17 +28,61 @@ function printId(id: number | string): void {
 // il se peut que typescrit ne connais pas le tye d un element
 // const compteur = document.querySelector("#compteur") as HTMLButtonElement; //  c' estpour indiquer a Typescreipt qu il s'agit d une Buutton HtML
 
-
-const compteur = document.querySelector("#compteur") as HTMLElement
-let i = 0
+const compteur = document.querySelector("#compteur") as HTMLElement;
+let i = 0;
 
 const increment = (e: Event) => {
-    e.preventDefault()
-    i++
-    const span = compteur?.querySelector("span")
-    if(span){
-        span.innerText = i.toString()
+   e.preventDefault();
+   i++;
+   const span = compteur?.querySelector("span");
+   if (span) {
+      span.innerText = i.toString();
+   }
+};
+
+compteur?.addEventListener("click", increment); //ajouter une "addEventListener" si compteur existe
+
+// le NARROWING : typescrit va devinner en foncstion du paramettre qu on a attribuer a une fonction
+
+function displayId(id: string | number) {
+   if (typeof id === "number") {
+      console.log(id * 3);
+   } else {
+      console.log(id.toUpperCase()); //ici typescript sait que a est un string car dans le prochain conditon c etait deja un number
+   }
+}
+
+// deuxieme exemple 
+function exemple(a: string | boolean, b: string | number | boolean){
+    if(a === b){
+        console.log(a); //doc ici typpescript va deiner que a serat soit "string | boolean" car ce sont les deux intercection entre a et b
     }
 }
 
-compteur?.addEventListener("click", increment) //ajouter une "addEventListener" si compteur existe
+// troisieme exemple cas d un tableau
+function autreExemple(a: string | string[]){
+    if(Array.isArray(a)){
+        return a[0] //tyescript va comprendre que a est ici un tableau
+    }
+    return
+}
+
+
+// prochain exemple 
+function prochainExemple(a: HTMLInputElement | MouseEvent){
+    if("value" in  a){
+        console.log(a); //ici typescript va comrendre que a est une "HTMLinputelement parce que value se truve dans une lement inpu en HTML "
+    }
+}
+
+// prochaine autre exemple 
+function isDate(a: any): a is Date{
+    return a instanceof Date
+}
+
+function example(a: Date | HTMLInputElement){
+    if (isDate(a)){
+        a //c est logique que a ici est une Date
+    }
+}
+
